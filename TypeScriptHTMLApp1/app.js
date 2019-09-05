c57cfea97fb7d43b55270c694d28bdcd2821446d
doronb226 <54857357+doronb226@users.noreply.github.com>
2019-09-05 15:51:49 +0300

    Add files via upload

---
+++ TypeScriptHTMLApp1/app.js
@@ -0,0 +1,8 @@
+var t = new Tester();
+debugger;
+t.test();
+//debugger
+//let g: Generator = new Generator();
+//g.getRandomItem().displayDetails();
+//g.getRandomItem().displayImg();
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,9 @@
+﻿
+let t: Tester = new Tester();
+debugger
+t.test();
+
+//debugger
+//let g: Generator = new Generator();
+//g.getRandomItem().displayDetails();
+//g.getRandomItem().displayImg();
